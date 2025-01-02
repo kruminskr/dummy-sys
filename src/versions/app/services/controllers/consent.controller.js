836cece54cbdd420305f6c45869b20a3ef6becb0
kruminskr <kruminskr@gmail.com>
2025-01-03 00:06:47 +0200

    feat: decopled consent signing

--- src/versions/app/services/controllers/consent.controller.js
+++ src/versions/app/services/controllers/consent.controller.js
@@ -5,7 +5,7 @@ const { generateToken } = require('../../../../services/token');
 var consentId; // varbút sim ir labaka alternativa
 var accountId;
 
-const createConsent = async (req, res) => {
+const redirectConsent = async (req, res) => {
     try {
         const consents = req.consents
 
@@ -18,12 +18,12 @@ const createConsent = async (req, res) => {
         const userIp = req.ip;
         const userAgent = req.headers['user-agent'];
 
+        const redirect = true;
+        const authorisation = false
+
         const consentDetails = req.body.consentDetails;
 
-
-        const consent = await consentModel.createConsent(bic, accessToken, date, reqId, userIp, userAgent, consentDetails);
-
-        console.log(consent)
+        const consent = await consentModel.createConsent(bic, accessToken, date, reqId, userIp, userAgent, consentDetails, redirect, authorisation);
 
         accountId = req.body?.accountId || consentDetails?.access?.availableAccounts;
         consents[accountId] = consent.consentId;
@@ -36,6 +36,84 @@ const createConsent = async (req, res) => {
         res.status(200).json(consent);
     } catch (error) {
         console.error(error?.response?.data?.tppMessages || error);
+        res.status(500).send(error?.response?.data?.tppMessages || error);
+    }
+}
+
+const decoupledConsent = async (req, res) => {
+    try {
+        const consents = req.consents
+
+        const accessToken = req.accessToken;
+        const refreshToken = req.refreshToken
+        const bic = req.bic
+
+        const date = new Date().toUTCString()
+        const reqId = 1; 
+        const userIp = req.ip;
+        const userAgent = req.headers['user-agent'];
+
+        const redirect = false;
+        const authorisation = true;
+
+        const consentDetails = req.body.consentDetails;
+
+        // create consent
+        const consent = await consentModel.createConsent(bic, accessToken, date, reqId, userIp, userAgent, consentDetails, redirect, authorisation);
+
+        // start authorisation
+        const consentData = await consentModel.startAuthorisation(bic, accessToken, consent.consentId, userIp, userAgent)
+
+        accountId = req.body.accountId;
+        consents[accountId] = consent.consentId;
+
+        const token = generateToken(accessToken, refreshToken, bic, consents);
+        consentData.token = token;
+
+        res.status(200).json(consentData);
+    } catch (error) {
+        console.error(error?.response?.data?.tppMessages || error);
+        res.status(500).send(error?.response?.data?.tppMessages || error);
+    }
+}
+
+// specify method and start SCA (Strong Customer Authentication)
+const startSCA = async (req, res) => {
+    try {
+        const accessToken = req.accessToken;
+        const bic = req.bic
+
+        const authorisationId = req.body.authorisationId
+        const body = req.body.info
+
+        const accountId = req.body.accountId;
+        const consentId = req.consents[accountId];
+
+        const scaData = await consentModel.startSCA(bic, accessToken, consentId, authorisationId, body)
+
+        return res.json(scaData);
+    } catch (error) {
+        console.error(error?.response?.data?.tppMessages || error);
+        res.status(500).send(error?.response?.data?.tppMessages || error);
+    }
+}
+
+const checkScaStatus = async (req, res) => {
+    try {
+        const accessToken = req.accessToken;
+        const bic = req.bic
+
+        const authorisationId = req.body.authorisationId
+
+        const accountId = req.body.accountId;
+        const consentId = req.consents[accountId];
+
+        await consentModel.checkScaStatus(accessToken, bic, consentId, authorisationId);
+
+        console.log('back')
+
+        return res.status(200).send()
+    } catch (error) {
         res.status(500).send(error?.response?.data?.tppMessages || error);
     }
 }
@@ -62,6 +140,9 @@ const handleConsentCallback = async (req, res) => {
 }
 
 module.exports = {
-    createConsent,
+    redirectConsent,
+    decoupledConsent,
+    startSCA,
+    checkScaStatus,
     handleConsentCallback, 
 }
--- src/versions/app/services/models/consent.model.js
+++ src/versions/app/services/models/consent.model.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const createConsent = async (bic, accessToken, date, reqId, userIp, userAgent, consentDetails) => {
+const createConsent = async (bic, accessToken, date, reqId, userIp, userAgent, consentDetails, redirect, authorisation) => {
     const params = {
         bic,
         'app-id': process.env.SWEDBANK_CLIENT_ID,
@@ -13,14 +13,102 @@ const createConsent = async (bic, accessToken, date, reqId, userIp, userAgent, c
         'PSU-IP-Address': userIp,
         'PSU-User-Agent': userAgent,
         'TPP-Redirect-URI': `${process.env.REDIRECT_URL}/api/app/consent/callback`,
-        'TPP-Redirect-Preferred': true,
-        'TPP-Explicit-Authorisation-Preferred': false,
+        'TPP-Redirect-Preferred': redirect,
+        'TPP-Explicit-Authorisation-Preferred': authorisation,
     };
 
     const { data } = await axios.post(`${process.env.SWEDBANK_API_URL}/sandbox/v5/consents`, consentDetails, {headers, params});
 
     return data;
 }
+
+const startAuthorisation = async (bic, accessToken, consentId, userIp, userAgent) => {
+    const date = new Date().toUTCString()
+    const reqId = 1; 
+
+    const params = {
+        bic,
+        'app-id': process.env.SWEDBANK_CLIENT_ID,
+    };
+
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+        Date: date,
+        'X-Request-ID': reqId,
+        'PSU-IP-Address': userIp,
+        'PSU-User-Agent': userAgent,
+        'TPP-Redirect-Preferred': 'false',
+    };
+
+    const { data } = await axios.post(`${process.env.SWEDBANK_API_URL}/sandbox/v5/consents/${consentId}/authorisations`, {}, {headers, params});
+
+    return data;
+}
+
+const startSCA = async (bic, accessToken, consentId, authorisationId, body) => {
+    const date = new Date().toUTCString()
+    const reqId = 1; 
+
+    const params = {
+        bic,
+        'app-id': process.env.SWEDBANK_CLIENT_ID,
+    };
+
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+        Date: date,
+        'X-Request-ID': reqId,
+
+    };
+
+    console.log('body', body);
+
+    const { data } = await axios.put(`${process.env.SWEDBANK_API_URL}/sandbox/v5/consents/${consentId}/authorisations/${authorisationId}`, body, {headers, params});
+
+    return data;
+}
+
+const getScaStatus = async (accessToken, bic, consentId, authorisationId) => {
+    const date = new Date().toUTCString()
+    const reqId = 1; 
+
+    const params = {
+        bic,
+        'app-id': process.env.SWEDBANK_CLIENT_ID,
+    };
+
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+        Date: date,
+        'X-Request-ID': reqId,
+    };
+
+    const { data } = await axios.get(`${process.env.SWEDBANK_API_URL}/sandbox/v5/consents/${consentId}/authorisations/${authorisationId}`, {headers, params});
+
+    return data;
+}
+
+
+const checkScaStatus = async (accessToken, bic, consentId, authorisationId) => {
+    try {
+        const poll = async () => {
+            const status = await getScaStatus(accessToken, bic, consentId, authorisationId);
+
+
+            if (status.scaStatus === 'finalised') {
+                return;
+            }
+
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            return poll(); 
+        };
+
+        await poll(); 
+    } catch (error) {
+        return error?.response?.data?.tppMessages || error
+    }
+};
+
 
 const getConsentStatus = async (bic, accessToken, date, reqId, consentId) => {
   const params = {
@@ -41,5 +129,8 @@ const getConsentStatus = async (bic, accessToken, date, reqId, consentId) => {
 
 module.exports = {  
     createConsent,
+    startAuthorisation,
+    startSCA,
+    checkScaStatus,
     getConsentStatus,
 };
